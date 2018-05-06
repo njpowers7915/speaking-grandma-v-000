02569447f73e7c3251df9902aeafc208c48bce35
Nicholas Powers <njpowers7915@gmail.com>
2018-05-06 00:45:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,8 +5,10 @@ def speak_to_grandma(phrase)
       return "I LOVE YOU TOO PUMPKIN!"
     else
       return "NO, NOT SINCE 1938!"
+    end
   else
     return "HUH?! SPEAK UP, SONNY!"
+  end
 end
 
 # Whatever you say to grandma, she should respond with
